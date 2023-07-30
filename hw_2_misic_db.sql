f17aa197df5824ffa308cd93a9e3381e15ebdfb1
Anton Gusev <antgusev@yandex.ru>
2023-07-30 21:24:19 +0500

    add table

--- hw_2_misic_db.sql
+++ hw_2_misic_db.sql
@@ -1,42 +1,47 @@
 CREATE TABLE IF NOT EXISTS Tracks (
-	id SERIAL PRIMARY KEY,
+	track_id SERIAL PRIMARY KEY,
 	album_id INTEGER NOT NULL REFERENCES album(id),
-	collection_id INTEGER REFERENCES collection(id),
 	name VARCHAR(40) NOT NULL,
 	length TIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Collections (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL,
-	year DATE NOT NULL
+	collection_id SERIAL PRIMARY KEY,
+	name VARCHAR(40) UNIQUE NOT NULL,
+	year DATE NOT NULL (year > 1900)
+);
+
+CREATE TABLE IF NOT EXISTS Track_collection (
+	tr_coll_id SERIAL PRIMARY KEY,
+    track_id INTEGER REFERENCES Tracks(id),
+	collection_id INTEGER REFERENCES Collections(id),
 );
 
 CREATE TABLE IF NOT EXISTS Albums (
 	id SERIAL PRIMARY KEY,
 	artist_id INTEGER NOT NULL REFERENCES artist(id),
-	name VARCHAR(40) NOT NULL,
-	year DATE NOT NULL
+	name VARCHAR(40) UNIQUE NOT NULL,
+	year DATE NOT NULL (year > 1900)
 );
 
 CREATE TABLE IF NOT EXISTS Album_artist (
-	id SERIAL PRIMARY KEY,
-    album_id INTEGER REFERENCES album(id),
-	artist_id INTEGER REFERENCES artist(id),
+	alb_art_id SERIAL PRIMARY KEY,
+    album_id INTEGER REFERENCES Albums(id),
+	artist_id INTEGER REFERENCES Artists(id),
 );
 
 CREATE TABLE IF NOT EXISTS Artists (
-	id SERIAL PRIMARY KEY,
+	artist_id SERIAL PRIMARY KEY,
 	name_nickname VARCHAR(40) NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS Musical_genres (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(40) NOT NULL,
+	genre_id SERIAL PRIMARY KEY,
+	name VARCHAR(40) UNIQUE NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS Genre_artist (
-	id SERIAL PRIMARY KEY,
-    genre_id INTEGER REFERENCES genre(id),
-	artist_id INTEGER REFERENCES artist(id),
+	art_gen_id SERIAL PRIMARY KEY,
+    genre_id INTEGER REFERENCES Musical_genres(id),
+	artist_id INTEGER REFERENCES Artists(id),
 );
